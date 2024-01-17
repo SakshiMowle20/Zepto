59de07ec6b9e792c4961ce9373d11d9447b5a64f
Sakshi Mowle <110199178+SakshiMowle20@users.noreply.github.com>
2024-01-17 18:14:08 +0530

    Initial commit in submodule

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [chips, setChips] = useState([]);
+  const [items, setItems] = useState([
+    'Nick Gian',
+    'Narayan Gamer',
+    'Anita Gros',
+    'Megan Smith',
+    'narayan murthi',
+  ]);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleItemClick = (item) => {
+    setChips([...chips, item]);
+    setItems(items.filter((i) => i !== item));
+    setInputValue('');
+  };
+
+  const handleChipRemove = (chip) => {
+    setChips(chips.filter((c) => c !== chip));
+    setItems([...items, chip]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="chip-container">
+      <div className="chips">
+        {chips.map((chip, index) => (
+          <div key={index} className="chip">
+            {chip}
+            <span
+              className="chip-remove"
+              onClick={() => handleChipRemove(chip)}
+            >
+              
+            </span>
+          </div>
+        ))}
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Type to search"
+          className="input"
+        />
+      </div>
+      {inputValue && (
+        <div className="item-list">
+          {items
+            .filter((item) =>
+              item.toLowerCase().includes(inputValue.toLowerCase())
+            )
+            .map((item, index) => (
+              <div
+                key={index}
+                className="item"
+                onClick={() => handleItemClick(item)}
+              >
+                {item}
+              </div>
+            ))}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
